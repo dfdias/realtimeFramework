f4897a161167d8c07b3088b7e634ab344c466b38
Duarte DIas <dfdias@ua.pt>
2020-10-10 10:02:43 +0100

    second

--- Channel.py
+++ Channel.py
@@ -1,34 +1,35 @@
 
-import dds_gen from dds_gen
+from dds_gen import dds_gen
 import numpy as np
 import scipy as sc
-class channel
+from scipy import constants as const
 
+class Channel:
 
-    def __init__ (self,Fs,Fc,N,ar,Fb,d1):
+    def __init__ (self,fs,fc,N,ar,fb,d1):
         #gets dds gen object initialized
-        self.Fc = Fc
-        self.Fs = Fs
+        self.fc = fc
+        self.fs = fs
         self.N = N
         self.ar = ar
-        self.Fb = Fb
+        self.fb = fb
         self.d1 = d1
-        
         self.Theta = 0
         self.d0 = 0 
         self.nd = 0 
         self.A0 = 1
         self.A1 = 1
         
-        self.gen = dds_gen(N,Fs,self.ar)
-        self.wavelen = sc.c/Fc       
-        self.t = np.arange(0,self.N-1)/self.Fs 
+        self.gen = dds_gen(N,self.fs,self.ar,self.fb)
+        self.wavelen = const.c/self.fc
+        self.t = np.arange(0,self.N-1)/self.fs
         self.theta1 = np.remainder(-4*np.pi*self.d1/self.wavelen,2*np.pi)
         
 
-    def evaluate(x)
-        b=self.gen2(self.N)
+    def evaluate(self,x):
+        b=self.gen.gen2(self.N)
         phase = np.remainder(-4*np.pi*self.d0/self.wavelen - self.Theta,2*np.pi)
         phase += -4*np.pi*b/self.wavelen
-        n = self.nd*(np.transpose(np.random.randn(1,self.N)) +np.transpose(1*j*np.random.randn(1,self.N)))
-        y = x*(self.A0*np.exp(1*j*phi) + self.A1*exp(1*j*self.theta1))+n
+        n = self.nd*(np.transpose(np.random.randn(1,self.N)) +np.transpose(1j*np.random.randn(1,self.N)))
+        y = x*(self.A0*np.exp(1j*phase) + self.A1*np.exp(1j*self.theta1))+n
+        return y
---
+++ main.py
@@ -0,0 +1,34 @@
+from Channel import Channel
+from dds_gen import dds_gen as gen
+import numpy as np
+from scipy import signal as sigs
+import time
+##made only for mathematical MODEL
+fc = 5.8e9
+f0 = 1e5
+fs = 1e5
+N = 1000
+ar = 0.003
+fb = 0.31
+d1 = 2
+sig = 0.01
+D = 1000
+T = 30
+
+brm = Channel(fs,fc,N,ar,fb,d1)
+brm.nd = 0.005
+brm.A0 = 0.1
+brm.A1 = 1
+brm.Theta = -np.pi/3
+brm.d0 = 1.009
+
+#sin generator
+sine1 = gen(N,fs,1,f0)
+
+#fir filter
+filt_coefs = sigs.firwin(500,1/D)
+nframes = np.round(fs*T/N).astype(np.int64)
+for nf in range (0,nframes):
+    s = sine1.gen(f0,N)
+    r = brm.evaluate(s)
+
