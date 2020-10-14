9e7fc539f1ca311c5500863df687c52a1aaf868a
Duarte DIas <dfdias@ua.pt>
2020-10-14 22:33:03 +0100

    circular buffer test script

--- fit_correct.m
+++ fit_correct.m
@@ -1,4 +1,4 @@
-function [f,x,y] = fit_correct(d,x_past,y_past,a1,a2,debug)
+function [f,x,y,r] = fit_correct(d,x_past,y_past,a1,a2,debug)
 
 g = d';
 XY = [real(g); imag(g)];%gera matriz para o hyperfix
@@ -7,7 +7,7 @@ x_present = P(1);
 y_present = P(2);
 x = a1*x_present + a2*x_past;
 y = a1*y_present + a2*y_past;
-
+r = P(3)
 g_x = XY(1,:)-x;%centra o arco na origem
 g_y = XY(2,:)-y;
 g = g_x + 1*j*g_y;%gera o sinal complexo
--- testcircularbuffer.m
+++ testcircularbuffer.m
@@ -1,4 +1,19 @@
 clear all
 clc
 
-ocb = circularbuffer(4,2)
+f= 10
+n = 2
+ocb = circularbuffer(f,n)
+
+for i = 1 : 40
+    a = randn(1,n)
+    ocb.appends(a)
+    idx = ocb.getorder()
+    buffer = ocb.buffer()
+    a = ocb.buffer(:,idx)
+    a(:)
+    pause
+end
+
+
+
