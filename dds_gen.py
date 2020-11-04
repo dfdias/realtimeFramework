dd6c3d2fe989f2fb8c0afcb8b397a48f471d1788
Duarte Dias <dfdias@ua.pt>
2020-11-04 09:45:48 +0000

    sliding window

--- dds_gen.py
+++ dds_gen.py
@@ -10,7 +10,13 @@ class dds_gen:
         self.f0 = f0
         self.cplx = cplx    #sets complex output
         self.IniPhase = 0
+        self.pastT = 0
         self.sintable = self.wavetable()
+        if n < fa/f0 :
+            print("insuficient resolution, using alternate method") 
+            self.low_freq = True
+        else :
+            self.low_freq = False
 
     def setIniPhase(self,phi):
         self.IniPhase = phi
@@ -45,28 +51,35 @@ class dds_gen:
             if phi > N-1:
                 phi = phi - N
         self.IniPhase = phi
-        x = x*self.ar
+        #x = x*self.ar
         return x
     
     
     def gen2(self):
         
-        N = self.N
-        #asserts that the ratio is a rational number
-        if self.cplx is True:
-            x = np.zeros(N, dtype=np.cdouble)
+        if self.low_freq is False :
+            N = self.N
+            #asserts that the ratio is a rational number
+            if self.cplx is True:
+                x = np.zeros(N, dtype=np.cdouble)
+            else:
+                x = np.zeros(N)
+            sintable = self.sintable
+            IncPhase = (self.f0/self.fs)*N 
+            n = 0
+            phi = self.IniPhase
+            for n in range(N) :
+                x[n] = sintable[int(np.round(phi))]
+                phi += IncPhase
+                if phi > N-1:
+                    phi = phi - N
+            self.IniPhase = phi
         else:
-            x = np.zeros(N)
-        sintable = self.sintable
-        IncPhase = (self.f0/self.fs)*N 
-        n = 0
-        phi = self.IniPhase
-        for n in range(N) :
-            x[n] = sintable[int(np.round(phi))]
-            phi += IncPhase
-            if phi > N-1:
-                phi = phi - N
-        self.IniPhase = phi
+            t = np.arange(self.pastT,self.pastT+self.N)/self.fs
+            x = self.ar*np.cos(2+np.pi*t*self.f0+self.IniPhase)
+            self.IniPhase = np.fmod(2*np.pi*self.f0*(self.pastT/self.fs)+self.IniPhase,2*np.pi)
+            self.pastT = t[-1]
+
         return x
     
     def gen_threaded(self, f0, NumSamp,q):
